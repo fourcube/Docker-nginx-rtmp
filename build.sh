40c92250cb3ec43efe1b00863ca5c74622384ff3
Chris Grieger <chris.grieger@rockstardevelopers.de>
2017-06-14 17:29:49 +0200

    update to nginx 1.12.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DOCKER_RUN_IMAGE=nginx-rtmp
+DOCKER_RUN_IMAGE=fourcube/nginx-rtmp
 DOCKER_BUILD_IMAGE=nginx-build
 
 rm -f nginx.tar.gz
@@ -21,7 +21,4 @@ docker rmi ${DOCKER_BUILD_IMAGE}
 
 docker build -t ${DOCKER_RUN_IMAGE} .
 
-docker images 
-docker run -d --name ${DOCKER_RUN_IMAGE} -p 1935:1935 -t ${DOCKER_RUN_IMAGE}
-docker ps -a
-
+docker push ${DOCKER_RUN_IMAGE}
